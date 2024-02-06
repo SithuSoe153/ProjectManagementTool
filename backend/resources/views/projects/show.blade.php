5dbe011487ec51f9cafac7fafddb2a3411e5a12c
soe <sithu032001@gmail.com>
2024-02-06 12:20:39 +0630

    task_card

--- backend/resources/views/projects/show.blade.php
+++ backend/resources/views/projects/show.blade.php
@@ -161,68 +161,95 @@
             {{-- CheckBox and Text Title Start --}}
             @forelse ($project->tasks as $task)
                 <div class="form-check">
-                    <input {{ $task->is_completed ? 'checked' : '' }} class="form-check-input task-checkbox"
-                        value="{{ $task->id }}" type="checkbox" value="" id="task-{{ $task->id }}">
-                    <label class="form-check-label" for="task-{{ $task->id }}">
-                        <span class="task-text {{ $task->is_completed ? 'task-completed' : '' }}">{{ $task->title }}
-                            |
-                            @foreach ($task->users as $user)
-                                {{ $user->name }}
-                            @endforeach
-                        </span>
+
+                    <div class="card">
+                        <h5 class="card-header">
 
 
-                        <a onclick="toggleFormAssign({{ $task->id }})" href="">
-
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-plus-circle" viewBox="0 0 16 16">
-                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                                <path
-                                    d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
-                            </svg>
-
-                        </a>
+                            <input {{ $task->is_completed ? 'checked' : '' }}
+                                class="form-check-input task-checkbox ms-0 me-2" value="{{ $task->id }}"
+                                type="checkbox" value="" id="task-{{ $task->id }}">
 
 
-                    </label>
+
+                            <span
+                                class="task-text {{ $task->is_completed ? 'task-completed' : '' }}">{{ $task->title }}
+                                |
+
+                                @foreach ($task->users as $user)
+                                    {{ $user->name }}
+                                @endforeach
+                            </span>
+
+
+
+                            <a onclick="toggleFormAssign({{ $task->id }})" href="" class="mx-2">
+
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                    fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                                    <path
+                                        d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+                                </svg>
+
+                            </a>
+
+                            <br>
+
+                            <span class="task-text {{ $task->is_completed ? 'task-completed' : '' }}">
+                                <small>Due Date: {{ $task->due_date }}</small>
+                            </span>
+
+                        </h5>
+
+
+                        <!-- Hidden Member Assign form initially -->
+                        <div class="container col-8" id="assignForm-{{ $task->id }}" style="display: none;">
+                            <div class="card-body">
+                                <script>
+                                    document.addEventListener("DOMContentLoaded", function() {
+                                        var membersSelect = new MultiSelectTag('members_{{ $task->id }}');
+                                    });
+                                </script>
+                                {{-- <div class="card"> --}}
+                                <form action="{{ route('task.assignMembers', $task->id) }}" method="POST"
+                                    class="card-body" id="assignMemberForm">
+                                    @csrf
+                                    <div class="form-group">
+                                        <h6 class="card-title">Assign Member</h6>
+                                    </div>
+
+                                    {{ $project->project_role_assignments->unique('user_id')->count() ? '' : 'No members assigned yet' }}
+
+                                    <div class="form-group">
+                                        <select name="members[]" id="members_{{ $task->id }}" multiple>
+                                            {{-- Populate options from roles table --}}
+                                            @foreach ($project->project_role_assignments->unique('user_id') as $assignment)
+                                                <option value="{{ $assignment->user->id }}">
+                                                    {{ $assignment->user->name }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    {{-- <button type="button" onclick="addAssignMember()" class="btn btn-primary my-3">Add
+                                    Member</button> --}}
+                                    <button type="submit" class="btn btn-success my-3">Assign Members</button>
+                                </form>
+                                {{-- </div> --}}
+                            </div>
+
+                            {{-- <h5 class="card-title">Special title treatment</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.
+                            </p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a> --}}
+                        </div>
+                    </div>
+
                 </div>
 
-                <p>Due Date: {{ $task->due_date }}</p>
 
                 {{-- CheckBox and Text Title End --}}
 
-                <!-- Hidden Member Assign form initially -->
-                <div class="container my-4 col-8" id="assignForm-{{ $task->id }}" style="display: none;">
-                    <script>
-                        document.addEventListener("DOMContentLoaded", function() {
-                            var membersSelect = new MultiSelectTag('members_{{ $task->id }}');
-                        });
-                    </script>
-                    <div class="card">
-                        <form action="{{ route('task.assignMembers', $task->id) }}" method="POST" class="card-body"
-                            id="assignMemberForm">
-                            @csrf
-                            <div class="form-group">
-                                <h5 class="card-title">Assign Member</h5>
-                            </div>
 
-                            {{ $project->project_role_assignments->unique('user_id')->count() ? '' : 'No members assigned yet' }}
-
-                            <div class="form-group">
-                                <select name="members[]" id="members_{{ $task->id }}" multiple>
-                                    {{-- Populate options from roles table --}}
-                                    @foreach ($project->project_role_assignments->unique('user_id') as $assignment)
-                                        <option value="{{ $assignment->user->id }}">
-                                            {{ $assignment->user->name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            {{-- <button type="button" onclick="addAssignMember()" class="btn btn-primary my-3">Add
-                                    Member</button> --}}
-                            <button type="submit" class="btn btn-success my-3">Assign Members</button>
-                        </form>
-                    </div>
-                </div>
 
                 <hr>
 
